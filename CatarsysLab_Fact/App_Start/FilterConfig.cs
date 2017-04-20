ff2b884f82afbaaaa8b4c2ad10ce9812e89c9daa
Mugauli <mugauli2@hotmail.com>
2017-04-20 00:26:57 -0500

    Agregar archivos de proyecto.

---
+++ CatarsysLab_Fact/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CatarsysLab_Fact
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CatarsysLab_Fact/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CatarsysLab_Fact.MvcApplication" Language="C#" %>
